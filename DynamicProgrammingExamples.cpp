23b3f5c90795b1ebff16ae2d66117196027924a4
Taseen Ariq <taseenariq06@gmail.com>
2025-06-02 16:46:25 -0400

    Update DynamicProgrammingExamples.cpp

--- DynamicProgrammingExamples.cpp
+++ DynamicProgrammingExamples.cpp
@@ -1,45 +1,53 @@
 #include <iostream>
 using namespace std;
 
-int fib(int n) {
+int fibonacci(int n) {
     static int memo[100] = {0}; // initialized to 0s
 
     if (n <= 1) return n;
     if (memo[n] != 0) return memo[n];
 
-    memo[n] = fib(n - 1) + fib(n - 2);
+    memo[n] = fibonacci(n - 1) + fibonacci(n - 2);
     return memo[n];
 }
 
-int fact (int n){
+int factorial (int n){ // factorial of a number
     static int memo[100] = {0};
 
     if(n <= 1) return 1;
     if(memo[n] != 0) return memo[n];
 
-    return memo[n] = n * fact(n-1);
+    return memo[n] = n * factorial(n-1);
 }
 
-int nat (int n){
+int natural (int n){ // add natural numbers 
     static int memo[100] = {0};
 
     if(n <= 1) return 1;
     if(memo[n] != 0) return memo[n];
 
-    return memo[n] = n + nat(n-1);
+    return memo[n] = n + natural(n-1);
 }
 
 int main() {
-    int n;
-    cout << "Enter n: ";
-    cin >> n;
-    cout << endl;
+    int a, b, c;
+    cout << "Enter a: ";
+    cin >> a;
     
-    for(int i = 0; i < n; i++){
-        cout << "fib(" << i << ") = " << fib(i) << endl;
+    cout << "Enter b: ";
+    cin >> b;
+    
+    cout << "Enter c: ";
+    cin >> c;
+    
+    cout << endl; 
+    
+    for(int i = 0; i < a; i++){
+        cout << "fibonacci(" << i << ") = " << fibonacci(i) << endl;
     }
+    cout << endl;
 
-    cout << fact(10) << endl;
-    cout << nat(10) << endl;
+    cout << "factorial(" << b << "): " << factorial(b) << endl;
+    cout << "natural(" << c << "): " << natural(c) << endl;
     return 0;
 } 
